983c3ba3669cfed622e122947a0c6a1d9275a630
Andrei Timotin <andreitimotin11@gmail.com>
2016-11-02 23:04:10 +0200

    Add model.php

--- www/view/view.php
+++ www/view/view.php
@@ -7,6 +7,29 @@
 </head>
 <body>
 <h1>Noutati</h1>
+<article>
+	<img src="">
+	<p class="title">Titlu stirei</p>
+	<p class="description">O descriere prompta a stirei in cauza. Nu trebuie sa ocupe foarte mult spatiu</p>
+</article>
+<article>
+	<img src="">
+	<p class="title">Titlu stirei</p>
+	<p class="description">O descriere prompta a stirei in cauza. Nu trebuie sa ocupe foarte mult spatiu</p>
+	<hr>
+</article>
+<article>
+	<img src="">
+	<p class="title">Titlu stirei</p>
+	<p class="description">O descriere prompta a stirei in cauza. Nu trebuie sa ocupe foarte mult spatiu</p>
+	<hr>
+</article>
+<article>
+	<img src="">
+	<p class="title">Titlu stirei</p>
+	<p class="description">O descriere prompta a stirei in cauza. Nu trebuie sa ocupe foarte mult spatiu</p>
+	<hr>
+</article>
 
 </body>
 </html>
